358e89f6ff339e2c32376bbbd0710d5ff4d6187e
akilblanchard <akiltb@hotmail.com>
2024-02-10 10:18:03 -0600

    Created infrastrcutre folder and configured Terraform provider file

---
+++ infrastructure/provider.tf
@@ -0,0 +1,15 @@
+#-----------------
+#AWS Provider
+#-----------------
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.32.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}

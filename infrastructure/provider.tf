1bc19252d4a17b4dd70906119b0115ca07e1edd8
akilblanchard <akiltb@hotmail.com>
2024-02-10 11:38:16 -0600

    Migrated tfstate to aws backend

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -8,8 +8,12 @@ terraform {
       version = "~>5.32.1"
     }
   }
-}
 
-provider "aws" {
-  region = "us-east-1"
+  backend "s3" {
+    bucket         = "767dev-tfstate-s3"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "dev-tfstate-db"
+  }
 }

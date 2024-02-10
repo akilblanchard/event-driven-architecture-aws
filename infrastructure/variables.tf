4d82c0f5904795e00e10140cd683901157afcc70
akilblanchard <akiltb@hotmail.com>
2024-02-10 11:06:02 -0600

    Ran terraform fmt and added Dynamobb vars to variable file

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,32 @@
 #s3  bucket variables
 
-variable "s3_bucket_name"{
-    description = "Name of the s3 bucket that holds the terraform state file"
-    type = "string"
-    default = "dev-tfstate-s3"
+variable "s3_bucket_name" {
+  description = "Name of the s3 bucket that holds the terraform state file"
+  type        = string
+  default     = "dev-tfstate-s3"
+}
+
+#DynamoDB Variables
+
+variable "tfstate_db_name" {
+  description = "Name of Database for state locking"
+  type        = string
+  default     = "dev-tfstate-db"
+}
+
+variable "hash_key" {
+  description = "Attribute Name"
+  type        = string
+  default     = "LockID"
+}
+
+variable "attr_name" {
+  description = "Attribute Name"
+  default     = "LockID"
+
+}
+
+variable "attr_type" {
+  description = "Attribute Type"
+  default     = "S"
 }

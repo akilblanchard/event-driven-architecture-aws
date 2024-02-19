46de7de85653440396bde7f847fc8f7973c1e633
akilblanchard <akiltb@hotmail.com>
2024-02-19 12:31:11 -0600

    Modified Configuration to point to variable names as opposed to hard coded values

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,18 +1,18 @@
 #Bucket to hold Terraform State File
 
 resource "aws_s3_bucket" "state_file" {
-  bucket = var.s3_bucket_name
+  bucket = var.bucket_name
 
   force_destroy = true
   tags = {
-    Nmae = "state_bucket"
+    Nmae = var.s3_bucket_tag
   }
 }
 
 #Versioning for State Bucket
 
 resource "aws_s3_bucket_versioning" "state_file" {
-  bucket = var.s3_bucket_name
+  bucket = var.bucket_name
 
   versioning_configuration  {
     status = "Enabled"
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_versioning" "state_file" {
 #Blcoking public access to the State File Bucket
 
 resource "aws_s3_bucket_public_access_block" "state" {
-  bucket                  = var.s3_bucket_name
+  bucket                  = var.bucket_name
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -32,17 +32,17 @@ resource "aws_s3_bucket_public_access_block" "state" {
 #Enable Server-Side encrytpion 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
-  bucket = var.s3_bucket_name
+  bucket = var.bucket_name
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = "aws_kms.state.arn"
-      sse_algorithm     = "aws:kms"
+      kms_master_key_id =  aws_kms_key.state.arn
+      sse_algorithm     = var.sse_algorithm 
     }
   }
 }
 
 resource "aws_kms_key" "state" {
   description             = "S3 Bucket encrytpion key"
-  deletion_window_in_days = 10
+  deletion_window_in_days = var.deletion_period
 }

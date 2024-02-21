a250baacd0da1fdf0c907ca3c94e033438747c2b
akilblanchard <akiltb@hotmail.com>
2024-02-21 15:29:50 -0600

    Added refrence to  Remote State module in provider

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -8,9 +8,19 @@ terraform {
       version = "~>5.32.1"
     }
   }
-  backend "s3" {}
+
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+module "remote_state" {
+  source      = "../modules/remote_state"
+  region = "us-east-1"
+  bucket_name = "767dev-tfstate-s3"
+  database_table = "dev-tfstate-db"
+  sse_algorithm = "aws:kms"
+  deletion_period = 30
+  s3_bucket_tag = "s3-Remote-State"
 }
